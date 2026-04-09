b88b4dfc9fd25a1d4ec58ad88553dee494737432
amiraq1 <amaralmdarking27@gmail.com>
2026-04-09 12:14:21 +0300

    feat: implement MLCEngine and add model download script

--- mlc4j/src/main/java/ai/mlc/mlcllm/MLCEngine.kt
+++ mlc4j/src/main/java/ai/mlc/mlcllm/MLCEngine.kt
@@ -56,6 +56,7 @@ class MLCEngine {
     fun reload(modelPath: String, modelLib: String) {
         val engineConfig = """
             {
+                "device": "vulkan",
                 "model": "$modelPath",
                 "model_lib": "system://$modelLib",
                 "mode": "interactive"
